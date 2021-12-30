948c90ac8e588e17fcf17ff2bec21fc978f908c5
Mehrshad Saadatinia <saadatiniamehrshad@gmail.com>
2021-12-31 02:00:47 +0330

    crud completed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'nodejs' },
@@ -16,14 +19,85 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
+
+
+
+
 app.get('/api/courses/:id', (req, res) => {
    const course = courses.find(c => c.id === parseInt(req.params.id));
    if (course === undefined) 
     {   
-       res.status(404).send('not found');
+       return res.status(404).send('not found');
     }
    res.send(course);
 });
+
+
+
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (course === undefined) 
+    {   
+       return res.status(404).send('not found');
+    }
+
+    const result = validateCourse(req.body);
+    
+    if (result.error !== undefined) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    //update course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+
+
+
+app.post('/api/courses', (req, res) => {
+ 
+    const result = validateCourse(req.body);
+
+    if (result.error !== undefined) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (course === undefined) 
+    {   
+       return res.status(404).send('not found');
+    }
+   //delete
+   const index = courses.indexOf(course);
+   courses.splice(index, 1);
+ 
+   //resp
+   res.send(course);
+});
+
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+    });
+    return schema.validate(course);
+}
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
