9f46b38ac114b7be0c0447a425f3deb9150f6f99
Mehrshad Saadatinia <saadatiniamehrshad@gmail.com>
2021-12-31 13:11:24 +0330

    wallet endpoints completed

--- src/app.js
+++ src/app.js
@@ -4,100 +4,143 @@ const Joi = require('joi');
 
 app.use(express.json());
 
-const courses = [
-    { id: 1, name: 'nodejs' },
-    { id: 2, name: 'django' },
-    { id: 3, name: 'phoenix' }
-];
+const wallets = [];
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
+    res.send('Hello!');
 });
 
 
 
 
-
-app.get('/api/courses/:id', (req, res) => {
-   const course = courses.find(c => c.id === parseInt(req.params.id));
-   if (course === undefined) 
-    {   
-       return res.status(404).send('not found');
-    }
-   res.send(course);
-});
-
-
-
-
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-   if (course === undefined) 
-    {   
-       return res.status(404).send('not found');
-    }
-
-    const result = validateCourse(req.body);
-    
-    if (result.error !== undefined) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-    //update course
-    course.name = req.body.name;
-    res.send(course);
-});
-
-
-
-
-app.post('/api/courses', (req, res) => {
- 
-    const result = validateCourse(req.body);
-
-    if (result.error !== undefined) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-
+app.get('/wallets', (req, res) => {
+    resp = {
+        size: wallets.length,
+        wallets: wallets,
+        code: 200,
+        message: "All wallets received successfully!"
     };
-    courses.push(course);
-    res.send(course);
+    res.send(resp);
 });
 
-app.delete('/api/courses/:id', (req, res) => {
-   const course = courses.find(c => c.id === parseInt(req.params.id));
-   if (course === undefined) 
+
+
+
+
+app.put('/wallets/:wname', (req, res) => {
+    const wallet = wallets.find(
+        w => w.name === req.params.wname
+        );
+
+    if (wallet === undefined) 
     {   
-       return res.status(404).send('not found');
+        return res.status(404).send('wallet not found');
     }
-   //delete
-   const index = courses.indexOf(course);
-   courses.splice(index, 1);
  
-   //resp
-   res.send(course);
+    const {error} = validateWallet(req.body);
+    if (error !== undefined) {
+         res.status(400).send(error.details[0].message);
+         return;
+    }
+    //update course
+    wallet.name = req.body.name;
+
+    resp = Object.assign({}, wallet);;
+    res.send(
+        Object.assign(resp, {
+            last_updated: constructTime(),
+            code: "200",
+            message: "Wallet name changed successfully!"
+        })
+        );
 });
 
 
-function validateCourse(course) {
+
+
+app.post('/wallets', (req, res) => {
+ 
+    const {error} = validateWallet(req.body);
+
+    if (error !== undefined) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    time = constructTime();
+    const wallet = {
+        name: req.body.name,
+        balance: 0.0,
+        coins: [],
+        last_updated: time
+    }; 
+    wallets.push(wallet);
+
+    resp = Object.assign({}, wallet);;
+    res.send(
+        Object.assign(resp, {
+            code: "200", 
+            message: "Wallet added successfully!"
+        })
+        );
+});
+
+
+
+
+
+app.delete('/wallets/:wname', (req, res) => {
+    const wallet = wallets.find(
+        w => w.name === req.params.wname
+        );
+
+    if (wallet === undefined) 
+    {   
+        return res.status(404).send('wallet not found');
+    }
+
+   //delete
+   const index = wallets.indexOf(wallet);
+   wallets.splice(index, 1);
+   
+   //resp
+   resp = Object.assign({}, wallet);;
+    res.send(
+        Object.assign(resp, {
+            last_updated: constructTime(),
+            code: "200", 
+            message: "Wallet deleted (logged out) successfully!"
+        })
+        );
+   
+});
+
+
+function validateWallet(wallet) {
     const schema = Joi.object({
-        name: Joi.string().min(3).required(),
+        name: Joi.string().min(1).required(),
     });
-    return schema.validate(course);
+    return schema.validate(wallet);
 }
 
+function constructTime() {
+    let date_ob = new Date();
 
+    let date = ("0" + date_ob.getDate()).slice(-2);
+
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    let year = date_ob.getFullYear();
+
+    let hours = date_ob.getHours();
+ 
+    let minutes = date_ob.getMinutes();
+    
+    let seconds = date_ob.getSeconds();
+
+    return (year + "-" + month + "-" + date + " " + hours + ":" + minutes);
+
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
